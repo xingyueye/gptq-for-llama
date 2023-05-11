416101b2ded508882830c5780252dc6e27e14a43
liliang58 <liliang58@meituan.com>
2023-05-11 15:22:16 +0800

    fix bugs of L100

--- utils/datautils.py
+++ utils/datautils.py
@@ -95,7 +95,7 @@ def get_c4(nsamples, seed, seqlen, model):
         while True:
             i = random.randint(0, len(valdata) - 1)
             tmp = tokenizer(valdata[i]['text'], return_tensors='pt')
-            if tmp.input_ids.shape[1] >= seqlen:
+            if tmp.input_ids.shape[1] >= seqlen+1:
                 break
         i = random.randint(0, tmp.input_ids.shape[1] - seqlen - 1)
         j = i + seqlen
